84fca4c643533275fdffd5dbe9c865b31ad2b6d6
KevinDaniel10 <kevin.cola@epn.edu.ec>
2024-10-27 15:32:49 -0500

    tab4

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  
 
   ngOnInit() {
   }
+  fechaInicio: string | null = null;
+  fechaFin: string | null = null;
+  resultado: number | null = null;
+
+  calcularDias() {
+    if (this.fechaInicio && this.fechaFin) {
+      const fechaInicioDate = new Date(this.fechaInicio);
+      const fechaFinDate = new Date(this.fechaFin);
+
+      const diferenciaTiempo = fechaFinDate.getTime() - fechaInicioDate.getTime();
+      this.resultado = Math.ceil(diferenciaTiempo / (1000 * 3600 * 24));
+    } else {
+      this.resultado = null;
+    }
+  }
+  
 
 }
